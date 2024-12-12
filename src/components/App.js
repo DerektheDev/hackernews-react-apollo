53f530866f2b988271f60a5b585dbf7220f36641
Derek Montgomery <hello@derekthe.dev>
2024-12-12 11:38:26 -0600

    Mutations: Creating Links, Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Header from './Header';
+import CreateLink from './CreateLink';
 import LinkList from './LinkList';
 
-class App extends Component {
-  render() {
-    return <LinkList />;
-  }
+
+const App = () => {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList/>} />
+          <Route
+            path="/create"
+            element={<CreateLink/>}
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './styles/index.css';
 import App from './components/App';
 
@@ -24,8 +26,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.createRoot(
   document.getElementById('root'),
   root.render(
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   )
 );
